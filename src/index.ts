a37a47e6f7da8b38fd37daa6ea225821c21e3d2b
Paulo Henrique <paulohenrique@esenvo.com>
2020-07-28 18:12:43 -0300

    Ajustando mensagem de erro

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-function keyExists(key:string):boolean{
-    return !(process.env[key] === undefined || (process.env[key] && process.env[key]!.toString().trim() === ''));
+function keyExists(key: string): boolean {
+  return !(
+    process.env[key] === undefined ||
+    (process.env[key] && process.env[key]!.toString().trim() === "")
+  );
 }
 
-export class MapEnv{
-    static get<T>(key:string):T {
-        if(!keyExists(key)){
-            throw new Error(`A variável de ambiente '${key}' não está definida.`);
-        }
-        return JSON.parse(process.env[key]!.toString()) as T;
+export class MapEnv {
+  static get<T>(key: string): T {
+    if (!keyExists(key)) {
+      throw new Error(`The environment variable '${key}' is not defined.`);
     }
+    return JSON.parse(process.env[key]!.toString()) as T;
+  }
 }

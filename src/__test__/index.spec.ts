84f16904152079e54d5ad4bf7ec4946f3400be4d
Paulo Henrique <paulohenrique@esenvo.com>
2020-07-29 15:57:57 -0300

    Add comments and tests

---
+++ src/__test__/index.spec.ts
@@ -0,0 +1,57 @@
+import { MapEnv } from "../index";
+
+describe("MapEnv functions T <String>", () => {
+  process.env.KEY = "VALUE";
+  process.env.JSON_KEY = '{ "VALUE": "OK" }';
+  process.env.JSON_KEY_INVALID = '{ "VALUE": "OK"';
+
+  it("'get' function => KEY2 not exists", () => {
+    var key = "KEY2";
+    expect(() => MapEnv.get<string>(key)).toThrow(
+      `The environment variable '${key}' is not defined.`
+    );
+  });
+
+  it("'get' function => KEY is not a string value", () => {
+    var key = "KEY";
+    expect(MapEnv.get<string>(key)).not.toEqual("VALUE");
+  });
+
+  it("'get' function => JSON_KEY valid", () => {
+    var key = "JSON_KEY";
+    var obj = { VALUE: "OK" };
+    expect(MapEnv.get<string>(key)).toEqual(obj);
+  });
+
+  it("'get' function => JSON_KEY invalid", () => {
+    var key = "JSON_KEY_INVALID";
+    expect(() => MapEnv.get<string>(key)).toThrow(
+      `The environment variable '${key}' is not a json format.`
+    );
+  });
+
+  it("'keyExists' function => KEY exists", () => {
+    var key = "KEY";
+    expect(MapEnv.keyExists(key)).toEqual(true);
+  });
+
+  it("'keyExists' function => KEY2 not exists", () => {
+    var key = "KEY2";
+    expect(MapEnv.keyExists(key)).toEqual(false);
+  });
+});
+
+describe("MapEnv functions T <Object>", () => {
+  class Obj {
+    value: string;
+  }
+
+  process.env.KEY = '{ "value": "OK"}';
+
+  it("'get' function => testing class obj", () => {
+    var key = "KEY";
+    const result = MapEnv.get<Obj>(key) as Obj;
+
+    expect(result.value).toEqual("OK");
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
-function keyExists(key: string): boolean {
-  return !(
-    process.env[key] === undefined ||
-    (process.env[key] && process.env[key]!.toString().trim() === "")
-  );
-}
-
+/**
+ * MapEnv -> Class to
+ */
 export class MapEnv {
+  /**
+   * Method to check is exists the environment variable
+   * @param {string} key variable name
+   */
+  static keyExists(key: string): boolean {
+    return !(
+      process.env[key] === undefined ||
+      (process.env[key] && process.env[key]!.toString().trim() === "")
+    );
+  }
+
+  /**
+   * Method to get the value from environment variable
+   * @param {string} key variable name
+   */
   static get<T>(key: string): T {
-    if (!keyExists(key)) {
+    if (!this.keyExists(key)) {
       throw new Error(`The environment variable '${key}' is not defined.`);
     }
-    return JSON.parse(process.env[key]!.toString()) as T;
+
+    try {
+      return JSON.parse(process.env[key]!.toString()) as T;
+    } catch {
+      throw new Error(
+        `The environment variable '${key}' is not a json format.`
+      );
+    }
   }
 }
